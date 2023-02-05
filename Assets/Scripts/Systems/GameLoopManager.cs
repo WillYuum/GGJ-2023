04ed4582ea668a98f5da72c8e2c06b6e52d661ea
Bogdan <bogdan.g.branescu@gmail.com>
2023-02-05 12:52:58 +0200

    Basic music layering

--- Assets/Scripts/Systems/GameLoopManager.cs
+++ Assets/Scripts/Systems/GameLoopManager.cs
@@ -9,6 +9,9 @@ public class GameLoopManager : MonoBehaviourSingleton<GameLoopManager>
     public bool LoopIsActive { get; private set; }
 
 
+    public float ClosestEnemyPositionY = -2.0f;
+
+
     private void Awake()
     {
         enabled = false;
@@ -22,6 +25,8 @@ public class GameLoopManager : MonoBehaviourSingleton<GameLoopManager>
         LoopIsActive = true;
 
         AudioManager.instance.PlayBGM("Strings");
+        AudioManager.instance.PlayBGM("Drums");
+        AudioManager.instance.PlayBGM("WindsAndStrings");
 
 
         if (OnGameLoopStart != null)
@@ -49,10 +54,7 @@ public class GameLoopManager : MonoBehaviourSingleton<GameLoopManager>
         });
     }
 
-
-    public void StartGameLoop()
-    {
-
-        AudioManager.instance.PlayBGM("Strings");
+    void Update() {
+        AudioManager.instance.setBGMMode(ClosestEnemyPositionY);
     }
 }
