0e959b4c97db81b5d65d2de2a34f736436279b1d
WillYum <liamshere98@gmail.com>
2023-02-05 15:26:55 +0200

    Set logic to activate roots

--- Assets/Scripts/GameLogic/Root.cs
+++ Assets/Scripts/GameLogic/Root.cs
@@ -8,10 +8,25 @@ public class Root : MonoBehaviour
     [SerializeField] private Sprite _rootOnSprite;
 
 
+    [SerializeField] private bool _activateOnLoad = false;
+
+
     public bool IsActive { get; private set; }
 
     [SerializeField] private SpriteRenderer _spriteRenderer;
 
+
+    private void Awake()
+    {
+        if (_activateOnLoad)
+        {
+            SwitchToActive();
+        }
+        else
+        {
+            SwitchToInactive();
+        }
+    }
 
     public void SwitchToActive()
     {
